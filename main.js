112c4b948653fa9c1eb3895cf6dfb385c7a98f85
Sebastian Castro <scastro@qvision.com.co>
2024-04-20 13:02:38 -0500

    [sc] second commit

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 
-function playSonidoPom() {
-    document.querySelector('#sonido_tecla_pom').play();
+// funtion play sonic
+function playSonido(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
+// access the element 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador< listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    console.log(contador)
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function(){
+        playSonido(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){ 
+        
+        if(evento.code ==='Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');  
+        } 
+        
+    };
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('activa');
+    };
+}
